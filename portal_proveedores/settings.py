7cc2df0e94888ab7cb96a7880941cc956a96f213
MariaMorales14 <myito1612@gmail.com>
2025-07-31 10:28:19 -0500

    Revert "Envio de correo de bienvenida al nuevo proveedor"
    This reverts commit 86055bc16661412d79b60348fac37439ba1b23bd.

--- portal_proveedores/settings.py
+++ portal_proveedores/settings.py
@@ -168,7 +168,7 @@ SESSION_EXPIRE_AT_BROWSER_CLOSE = True
 SESSION_COOKIE_AGE = 86400 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # settings.py
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
