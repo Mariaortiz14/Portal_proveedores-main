08103eea53e7d32c58650751683fbe9802358334
MariaMorales14 <myito1612@gmail.com>
2025-08-01 08:56:23 -0500

    Revert "Activacion de recuperacion de contraseña buscando al proveedor en la base de datos, si no lo encuentra, no lo deja, las demas vistas la contraseña sera restablecida o cambiada por medio del perfil de administrador"
    This reverts commit 964dc7a369c79fa820e353b84d0119e644e420ce.

--- users/urls.py
+++ users/urls.py
@@ -2,20 +2,15 @@ from django.urls import path
 from . import views
 
 app_name = 'users'
-
 urlpatterns = [
     path('actividad_economica/', views.actividad_economica, name='actividad_economica'),
     path('get_municipios/', views.get_municipios, name='get_municipios'),
-    
-    path('login/', views.login_, name='login'),
-    path('signup/', views.signup, name='signup'),
-    path('register/', views.signup, name='register'),
-    path('logout/', views.logout_, name='logout'),
-
-    # Recuperación de contraseña
-    path('olvide-contrasena/', views.forgot_password, name='forgot_password'),
-    path('restablecer-contrasena/<str:username>/', views.reset_password, name='reset_password'),
-
-    # Perfil
+    path('profile/', views.profile, name='administracion'),
+    path('profile/', views.profile, name='comprador'),
+    path('profile/',views.profile,name='logistica'),
     path('profile/', views.profile, name='profile'),
+    path('logout/', views.logout_, name='logout'),
+    path('signup/', views.signup, name='signup'), 
+    path('login/', views.login_, name='login'),
+    path('register/', views.signup, name='register'),
 ]
